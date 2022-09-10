5f5cf310066611e06d8a0a28283cb10b7cf057d0
erumi321 <42208651+erumi321@users.noreply.github.com>
2022-09-10 12:09:11 -0400

    fix db not in newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -1,3 +1,18 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDZ-ZDUwAxvd2Hv4x3YCN4O7bGHykUFKmI",
+    authDomain: "paulprogrammernewsletter.firebaseapp.com",
+    projectId: "paulprogrammernewsletter",
+    storageBucket: "paulprogrammernewsletter.appspot.com",
+    messagingSenderId: "546822540387",
+    appId: "1:546822540387:web:12bcfb7a5dcdec49f47c56"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+
 var url = document.URL;
 
 var uid = url.split("?")[1].split(",")[0];
